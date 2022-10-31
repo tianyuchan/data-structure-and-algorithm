bbf85911eadae68c9aa92c90afe1b238d88fa12a
tianyuchan <tianyuchenc@163.com>
2022-10-31 22:36:24 +0800

    update

--- src/main/java/math/bitwise/BinaryNumberWithAlternatingBits.java
+++ src/main/java/math/bitwise/BinaryNumberWithAlternatingBits.java
@@ -13,6 +13,9 @@ public class BinaryNumberWithAlternatingBits {
     /**
      * 错位异或法
      *
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.4 MB, 在所有 Java 提交中击败了48.68%的用户
+     * 通过测试用例：204 / 204
      * @param n
      * @return
      */
